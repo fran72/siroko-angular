1e47f69fe450a960bfd36264287417565132ed56
Fran72 <francisco_llorente@hotmail.com>
2024-11-29 16:05:05 +0100

    radion buttons color white falta label y ripple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatRadioModule} from '@angular/material/radio';
+// import { FormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatRadioModule],
+  imports: [RouterOutlet, MatRadioModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'siroko-angular';
+
+  questions = [ 'siroko-angular', 'siroko-angular','siroko-angular','siroko-angular','siroko-angular','siroko-angular','siroko-angular','siroko-angular','siroko-angular' ];
 }
