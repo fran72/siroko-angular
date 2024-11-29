e331eae4423dc07e1b1f20edd2d1c5fb5d7ba252
Fran72 <francisco_llorente@hotmail.com>
2024-11-29 13:45:31 +0100

    radio buttons sin funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatRadioModule} from '@angular/material/radio';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatRadioModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
