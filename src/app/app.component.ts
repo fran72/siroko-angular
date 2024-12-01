cb9e08569655ea2d7b04f5ff294f0f5c6ef80fee
Fran72 <francisco_llorente@hotmail.com>
2024-12-01 13:27:22 +0100

    checked radio button funciona - obligatorio value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatRadioModule} from '@angular/material/radio';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,12 +16,14 @@ interface Time {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   time: any;
   timerId: number | countdown.Timespan = 0;
 
   step: number = 0;
 
+  selectedOption: string = '2016';
   selectedWizardStep0: string = '';
   selectedWizardStep1: string = '';
 
@@ -40,14 +42,19 @@ export class AppComponent {
   ];
 
   wizardOptions = [
-    {id: 0, questions: [ '2016', '2017','2018','2019','2020','2021' ]},
+    {id: 0, questions: [ '2016', '2017', '2018', '2019', '2020', '2021' ]},
     {id: 1, questions: [ 'Segaría a navaja', 'Rechazaría un cachopo', 'Renunciaría a mis tierras', 'Regalaría una ternera' ]},
   ];
 
   countDownActivated: boolean = false;
   countDownFinished: boolean = false;
 
-
+  ngOnInit() {
+    if (this.step === 0) this.selectedOption = this.wizardOptions[this.step].questions[0];
+    console.log('this.step', this.step);
+    console.log('this.selectedOption', this.selectedOption);
+  }
+  
   setStep(step: number) {
     this.step = step;
 
@@ -57,6 +64,9 @@ export class AppComponent {
   }
 
   onSelect(option: string){
+    console.log('onSelect');
+    this.selectedOption = option;
+
     if (this.step === 0) {
       this.selectedWizardStep0 = Number(+option[option.length-1] + +option[option.length-2]).toString();
       console.log(this.selectedWizardStep0);
@@ -64,7 +74,6 @@ export class AppComponent {
       var cleanedString = option.replace(/ /g, '').replace(/a/g, '').replace(/A/g, '');
       this.selectedWizardStep1 = cleanedString.substring(cleanedString.length - 4, cleanedString.length);
       console.log(this.selectedWizardStep1);
-
     }
   }
 
@@ -104,6 +113,41 @@ export class AppComponent {
       console.error('Error al copiar: ', err);
     }
   }
+
+  isItemChecked(step: number, question: string) {
+
+    if (step === this.step && question ===  this.selectedOption) {
+      console.log('isSelected    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', true);
+      return true;
+    } else {
+      console.log('isNotSelected    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', false);
+      return false;
+    }
+  }
+
+  isSelected(step: number, question: string) {
+
+    if (step === this.step && question ===  this.selectedOption) {
+      console.log('isSelected    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', true);
+      return true;
+    } else {
+      console.log('isNotSelected    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', false);
+      return false;
+    }
+  }
+
+  // isChecked(step: number, question: string) {
+  //   this.selectedOption = this.wizardOptions[this.step].questions[0];
+
+  //   if (step === this.step && question === this.selectedOption) {
+  //     console.log('isChecked    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', true);
+  //     return true;
+  //   } else {
+  //     console.log('isNotChecked    ', step ,' ',  this.step,' ', question ,'....', this.selectedOption ,'....', false);
+  //     return false;
+  //   }
+  // }
+
 
   ngOnDestroy(): void {
     if (this.timerId) {
